14791d252454070a50377c3b3de575bb8d87a141
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-08-04 10:41:09 +0530

    implemeneted helper function to reverse the array

--- arrays.py
+++ arrays.py
@@ -10,6 +10,23 @@ def rotateLeft(arr, n):
     arr[:] = reversed(arr)
     print(arr)
 
-x = [1, 2, 3, 4, 5, 6]
-n = 3
-rotateLeft(x, n)  # Output: [4, 5, 6, 1, 2, 3]
+# x = [1, 2, 3, 4, 5, 6]
+# n = 3
+# rotateLeft(x, n)  # Output: [4, 5, 6, 1, 2, 3]
+
+
+def rotate_Left(arr,n):
+    size=len(arr)
+    if n==0:
+        return
+    n=n%size
+
+    def reverse(arr,left, right):
+        while left>=right:
+            arr[left],arr[right]=arr[right],arr[left]
+            left+=1
+            right-=1
+
+    reverse(arr,0,n)
+    reverse(arr,n+1,size)
+    reverse(arr,0,size)
