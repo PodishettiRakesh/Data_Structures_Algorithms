ab96863728c5805cfada48a050818716ebd45a26
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-08-21 22:03:42 +0530

    code to find the union of two arrays

--- arrays.py
+++ arrays.py
@@ -62,7 +62,35 @@ def moveZerosToEnd(arr):
             arr[i],arr[j]=arr[j],arr[i]
             j+=1
     
-x=[1,0,2,0,3,4,0]
-moveZerosToEnd(x)
-print(x)
+# x=[1,0,2,0,3,4,0]
+# moveZerosToEnd(x)
+# print(x)
+
+
+
+def find_union(arr1, arr2):
+    freq = {}
+    union = []
+    
+    for num in arr1:
+        freq[num] = freq.get(num, 0) + 1
+        print(freq)
+    
+    for num in arr2:
+        freq[num] = freq.get(num, 0) + 1
+        print("arr2", freq)
+    
+    for num in freq:
+        union.append(num)
+    
+    return union
+
+arr1 = [1, 2, 3, 4, 5, 6,6,6, 7, 8, 9, 10]
+arr2 = [2, 3, 4, 4, 5, 11, 12]
+
+union = find_union(arr1, arr2)
+
+print("Union of arr1 and arr2 is:")
+for num in union:
+    print(num, end=" ")
 
