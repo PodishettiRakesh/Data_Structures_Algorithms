05e3b2b30cbfbd88137415ade6221c5dd8950573
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-08-30 19:14:27 +0530

    second smallest element from the given array  without sorting

--- ArraysEasy.java
+++ ArraysEasy.java
@@ -17,10 +17,29 @@ public class ArraysEasy {
             return secondLargest;
         }
     }
+
+    public int findSecondSmallest(int[] arr){
+        int small=Integer.MAX_VALUE;
+        int secondSmall=Integer.MAX_VALUE;
+        for(int num: arr){
+            if(num<small){
+                secondSmall=small;
+                small=num;
+            }else if (num<secondSmall && num!=small){
+                secondSmall=num;
+            }
+        }
+        if(secondSmall==Integer.MAX_VALUE){
+            return -1;
+        }else{
+            return secondSmall;
+        }
+        
+    }
     public static void main(String[] args) {
-        int[] arr={11,12,23,2,14,45,34};
+        int[] arr={11,12,23,14,45,34};
         ArraysEasy obj= new ArraysEasy();
         System.out.println(obj.findSecondLargest(arr));
-
+        System.out.println(obj.findSecondSmallest(arr));
     }
 }
