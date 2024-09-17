9383570f75abba21521e233d012cbdc59fe7baea
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-09-17 19:43:19 +0530

    removeDuplicatesInSorted is implemented using two pointers technique

--- ArraysEasy.java
+++ ArraysEasy.java
@@ -57,7 +57,14 @@ public class ArraysEasy {
     }
 
     public int removeDuplicatesInSorted(int arr[]){
-
+        int i=0;
+        for(int j=1; j<arr.length; j++){
+            if(arr[i]!=arr[j]){
+                arr[i+1]=arr[j];
+            }
+            i++;
+        }
+        return i+1;
     }
 
     public static void main(String[] args) {
