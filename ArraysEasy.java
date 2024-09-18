390b781f12fcaa8d8a092acf6f02f4f4eb98e8fe
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-09-18 18:25:04 +0530

    implemented swap helper method for sorting012 to swap the numbers

--- ArraysEasy.java
+++ ArraysEasy.java
@@ -157,20 +157,34 @@ public class ArraysEasy {
         System.out.println(Arrays.toString(newArray));
     }
 
+    public void swap(int[] arr, int low, int high){
+        int temp=arr[low];
+        arr[low]=arr[high];
+        arr[high]=temp;
+    }
     public void sort012(int[] arr){
-        for(int nums:arr){
-            System.out.println(nums);
-        }
+        int low=0, mid=0, high=arr.length-1;
+            while (mid<=high) {
+                if(arr[mid]==0){
+                    swap(arr, mid, low);
+                    mid++;
+                    low++;
+                }else if (arr[mid]==1){
+                    mid++;
+                }else{
+                    swap(arr, mid, high);
+                    high--;
+                }
+            }
+        
+        System.out.println(Arrays.toString(arr));
     }
 
 
     public static void main(String[] args) {
-        int[] arr={1,2,3,5};
-        int[] arr1={1,3,6};
-        int[] arr2={2,4,5,7};
+        int[] arr2={0,1,1,2,0,0};
         ArraysEasy obj= new ArraysEasy();
-        System.out.println(obj.findMissing(arr));
-        obj.mergeTwoSorted(arr1, arr2);
+        obj.sort012(arr2);
 
     }
 }
