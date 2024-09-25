57ffd3dacd12867370a3bf0e614ed919b536cbbd
PodishettiRakesh <podishettirakesh70@gmail.com>
2024-09-25 10:21:13 +0530

    insertNode method to insert node in current BST Tree

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,20 +1,44 @@
-class Node{
-    int key;
-    Node left;
-    Node right;
 
-    public Node(int item){
-        key=item;
-        left=right=null;
-    }
-}
 public class BinarySearchTree {
+    class Node{
+        int key;
+        Node left;
+        Node right;
+    
+        public Node(int item){
+            key=item;
+            left=right=null;
+        }
+    }
     Node root;
 
     public BinarySearchTree(){
         root=null;
     }
+
+    public void insertNode(int data){
+        root=insertNodeabs(root, data);
+    }
+
+    public Node insertNodeabs(Node root, int data){
+        if(root==null){
+            root=new Node(data);
+            return root;
+        }
+
+        if(root!=null){
+            if(data<root.key){
+                insertNodeabs(root.left, data);
+            }else if(data>root.key){
+                insertNodeabs(root.right, data);
+            }
+        }
+        return root;
+    }
+
     public static void main(String[] args) {
-        
+        BinarySearchTree BSTtree=new BinarySearchTree();
+        System.out.println(BSTtree.root);
+        BSTtree.insertNode(5);
     }
 }
